944088f3385c2ace9612d5759fd38b767d224afb
Ryan Killeen <rykilleen@gmail.com>
2022-01-13 01:20:42 -0500

    update prisma to create less connections

--- pages/api/items.ts
+++ pages/api/items.ts
@@ -1,27 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Item, PrismaClient } from '@prisma/client'
+import { Item, PrismaClient } from "@prisma/client";
 
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-
+const prisma = new PrismaClient();
 export const getItems = async () => {
-    const prisma = new PrismaClient()
-    const items = await prisma.item.findMany()
-  
-    // const categoriedItems = groupBy(items, 'category')
-    return items
-}
-
-
-
+  const items = await prisma.item.findMany();
+  return items;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-const prisma = new PrismaClient()
   const items = await getItems();
-  
-  res.status(200).json(items)
-
+  res.status(200).json(items);
 }
--- pages/api/update-item.ts
+++ pages/api/update-item.ts
@@ -1,26 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from '@prisma/client'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-
+const prisma = new PrismaClient();
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { item } = req.body
-
-
-  const prisma = new PrismaClient()
+  const { item } = req.body;
 
   const results = await prisma.item.update({
     where: {
-      id: item.id
+      id: item.id,
     },
     data: {
-      ...item
-    }
-  })
+      ...item,
+    },
+  });
 
-  res.status(200).send(results)
-
+  res.status(200).send(results);
 }
